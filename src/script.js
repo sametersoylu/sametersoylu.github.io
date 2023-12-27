e9ee66eb0baa294db7927bc39083bdbc98b20a9e
Abdüssamet <abdussametersoylu@gmail.com>
2023-12-27 21:18:44 +0200

    Updated

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ let url = window.location.href;
 let userid = "example"; 
 let username = "Example"; 
 if(url.includes(".github.io")) {
-    userid = url.replace(".github.io", ""); 
+    userid = url.replace(".github.io", "").replace("/", "").replace("https:", "").replace("www");
 }
 let repolist = document.getElementById("repolist"); 
 $.getJSON(`https://api.github.com/users/${userid}/repos`, function(data){
@@ -13,6 +13,8 @@ $.getJSON(`https://api.github.com/users/${userid}/repos`, function(data){
         repolist.appendChild(createRepoCard(data[i]))
     }
 })
+
+$.getJSON(``)
 
 
 function createRepoCard(data) {
